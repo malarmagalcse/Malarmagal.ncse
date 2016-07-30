7e78f5b9548037137f199ddb5f815a0331f3088e
malarmagalcse <malarmagal.ncse@ksrce.ac.in>
2016-07-30 18:25:55 +0530

    Update PrintNumbers.java

--- PrintNumbers.java
+++ PrintNumbers.java
@@ -2,9 +2,9 @@ package day.one;
 
 public class PrintNumbers {
 	int i=1;
-	public void printNum(){
+	public void printNum(int n){
 		
-		if(i<=100){
+		if(i<=n){
 			System.out.println(""+i);
 			i++;
 			printNum();
@@ -14,7 +14,7 @@ public class PrintNumbers {
 
 	public static void main(String[] args) {
 		PrintNumbers printNumbers = new PrintNumbers();
-		printNumbers.printNum();
+		printNumbers.printNum(100);
 
 	}
 
