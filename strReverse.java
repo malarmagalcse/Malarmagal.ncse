5256498de668b567cb5c2d557aeab0dfcf95cbb3
malarmagalcse <malarmagal.ncse@ksrce.ac.in>
2016-08-18 14:12:47 +0530

    Create strReverse.java

---
+++ strReverse.java
@@ -0,0 +1,12 @@
+package Guvi;
+
+public class strReverse {
+
+	public static void main(String[] args) {
+		StringBuffer str=new StringBuffer();
+		str.append(args[0]);
+		StringBuffer str1=new StringBuffer(str);
+		str1=str1.reverse();
+		System.out.println(str1);
+	}
+}
