0a6478e2435635bbadb44788ce673ba309a48a3d
malarmagalcse <malarmagal.ncse@ksrce.ac.in>
2016-08-24 18:04:35 +0530

    Create CheckSquare.java

---
+++ CheckSquare.java
@@ -0,0 +1,20 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class CheckSquare {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int[] a = new int[8];
+		System.out.println("enter 4 points as (x,y) coordinates");
+		for (int i = 0; i < 8; i++) {
+			a[i] = sc.nextInt();
+		}
+		if (a[0] == a[2] && a[4] == a[6] && a[1] == a[7] && a[3] == a[5])
+			System.out.println("this coodinates can form square");
+		else
+			System.out.println("not an square making points");
+	}
+}
